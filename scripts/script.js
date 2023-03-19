beb078a6b648f147da7c296129e8fd88e9b24d96
Nicolas <127538225+Nicode611@users.noreply.github.com>
2023-03-19 22:59:54 +0100

    2259

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,14 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
   logo.classList.toggle('show');
 });
+
+
+const rangeInput = document.querySelector('.form-range');
+const personNumber = document.querySelector('.person-number');
+
+rangeInput.addEventListener("input", function() {
+  var rangeValue = rangeInput.value;
+  (console.log(rangeValue))
+});
+
+personNumber.innerText = rangeValue;
