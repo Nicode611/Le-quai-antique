2db1ec78e4fed72d664b91d0cdcc76f5b50c7fc5
Nicolas <127538225+Nicode611@users.noreply.github.com>
2023-03-22 00:33:05 +0100

    00:33

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ hamburger.addEventListener('click', () => {
   logo.classList.toggle('show');
 });
 
+//----------------------------------------------------------------------
 
 const rangeInput = document.querySelector('.form-range');
 const personNumber = document.querySelector('.person-number');
@@ -15,3 +16,20 @@ rangeInput.addEventListener("input", function() {
   var rangeValue = rangeInput.value;
   personNumber.innerText = rangeValue;
 });
+
+//----------------------------------------------------------------------
+const dateInput = document.querySelector('.input-date');
+const myFieldset = document.getElementById("form-btn-fieldset");
+
+const isValid = () => {
+  if (rangeInput.value != 0 && dateInput.value !== '') {
+    console.log('oui');
+    myFieldset.disabled = false;
+    
+  }
+}
+
+rangeInput.addEventListener('change', isValid);
+dateInput.addEventListener('change', isValid);
+
+//----------------------------------------------------------------------
