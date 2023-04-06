e1142e17c173295c69d1436280645357592c998f
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-04-06 16:22:32 +0200

    16:22

--- scripts/dashboard-admin-script.js
+++ scripts/dashboard-admin-script.js
@@ -1,17 +1,40 @@
 //---------------------------------Active catégorie dashboard admin-------------------------------------
 
-const reservationsCat = document.querySelector('.dash-categories.dash-reservations');
+const reservationsCat = document.querySelector('.dash-reservations');
 const galerieCat = document.querySelector('.dash-galerie');
+const carteCat = document.querySelector('.dash-carte');
+const horairesCat = document.querySelector('.dash-horaires');
 
 const reservationsContent = document.querySelector('.dash-reservations-main-content');
 const galerieContent = document.querySelector('.dash-galerie-main-content');
+const carteContent = document.querySelector('.dash-carte-main-content');
+const horairesContent = document.querySelector('.dash-horaires-main-content');
 
 galerieCat.addEventListener('click', () => {
   galerieContent.style.display = 'flex';
   reservationsContent.style.display = 'none';
+  carteContent.style.display = 'none';
+  horairesContent.style.display = 'none';
 });
 
 reservationsCat.addEventListener('click', () => {
   galerieContent.style.display = 'none';
   reservationsContent.style.display = 'flex';
+  carteContent.style.display = 'none';
+  horairesContent.style.display = 'none';
 });
+
+carteCat.addEventListener('click', () => {
+    galerieContent.style.display = 'none';
+    reservationsContent.style.display = 'none';
+    carteContent.style.display = 'flex';
+    horairesContent.style.display = 'none';
+});
+
+horairesCat.addEventListener('click', () => {
+    galerieContent.style.display = 'none';
+    reservationsContent.style.display = 'none';
+    carteContent.style.display = 'none';
+    horairesContent.style.display = 'flex';
+});
+  
